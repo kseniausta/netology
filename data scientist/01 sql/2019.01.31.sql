1e4ae0f2c95b7e8eec7378a5d826878789f26f6c
kseniausta <47122787+kseniausta@users.noreply.github.com>
2019-02-05 23:27:06 +0300

    Update 2019.01.31.sql

--- data scientist/01 sql/2019.01.31.sql
+++ data scientist/01 sql/2019.01.31.sql
@@ -1,1 +1,12 @@
 SELECT 'ФИО: Уставщикова Ксения Сергеевна' as name;
+
+-- Оконные функции.
+
+SELECT userid, movieid, 
+(rating - MIN(rating) OVER (PARTITION BY userid))/(MAX(rating) OVER (PARTITION BY userid) - MIN(rating) OVER (PARTITION BY userid)) normed_rating, 
+AVG(rating) OVER (PARTITION BY userid) avg_rating
+FROM ratings r
+LIMIT 30
+;
+
+                                                                                                              
