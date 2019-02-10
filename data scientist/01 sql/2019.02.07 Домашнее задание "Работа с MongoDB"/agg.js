8f2bbee94b973078bf4c41f79f62b0cf1b9cfe27
kseniausta <47122787+kseniausta@users.noreply.github.com>
2019-02-10 20:48:52 +0300

    Update agg.js

--- data scientist/01 sql/2019.02.07 Домашнее задание "Работа с MongoDB"/agg.js
+++ data scientist/01 sql/2019.02.07 Домашнее задание "Работа с MongoDB"/agg.js
@@ -1,24 +1,23 @@
 /*
         Написать запрос, который выводит общее число тегов
 */
-print("tags count: ", 'расчёт количества тегов');
+print("tags  count: ", db.tags.find().count());
 /*
         Добавляем фильтрацию: считаем только количество тегов woman
 */
-print("woman tags count: ", 'расчёт количества тегов woman');
+print("tags  count: ", db.tags.find({'name': 'woman'}).count());
 /*
         Очень сложный запрос: используем группировку данных посчитать количество вхождений для каждого тега
         и напечатать top-3 самых популярных
 */
 
 printjson(
-        db.tags.aggregate([
-                {"$group": {
-                                "--": "--",
-                                "--": "--"
-                           }
-                },
-                {"тут модификатор sort по полю с аггрегацией"},
-                {$limit: 3}
-        ])['_batch']
+		db.tags.aggregate([
+			{"$group": {
+						_id: {name: '$name'},
+						count: {$sum: 1}
+					}
+			},
+			{$sort: {count: -1}},{$limit: 3}
+		])['_batch']
 );
